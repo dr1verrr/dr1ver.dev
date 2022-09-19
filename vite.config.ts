9dfa106d3cc76847baf295d50e3781f5d555c6e0
dr1verrr <darfor2107@gmail.com>
2022-09-19 16:24:07 +0300

    Init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  root: './',
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  },
+  css: {
+    postcss: './postcss.config.js'
+  },
+  define: {
+    'process.env': process.env
+  },
+  publicDir: 'public',
+  build: {
+    chunkSizeWarningLimit: 1600
+  }
+})
