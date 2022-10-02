aa1941f5f6bb1fdc4c76be3f7f761ef2ed997e2d
dr1verrr <darfor2107@gmail.com>
2022-10-02 13:11:23 +0300

    feat(services): add emailjs constants

---
+++ src/services/emailjs/constants.ts
@@ -0,0 +1,7 @@
+const EMAILJS = {
+  templateId: import.meta.env.VITE_APP_TEMPLATE_ID,
+  serviceId: import.meta.env.VITE_APP_SERVICE_ID,
+  publicKey: import.meta.env.VITE_APP_PUBLIC_KEY
+}
+
+export { EMAILJS }
