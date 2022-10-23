508c5161ec76a95f3e8cad53ec461685a6660d43
dr1verrr <darfor2107@gmail.com>
2022-10-23 20:16:17 +0300

    refactor: remove routes

--- src/components/routes/constants.ts
+++ src/components/routes/constants.ts
@@ -4,7 +4,6 @@ const Projects = lazy(() => import('@/views/Projects'))
 const Skills = lazy(() => import('@/views/Skills'))
 const About = lazy(() => import('@/views/About'))
 const Contact = lazy(() => import('@/views/Contact'))
-const Test = lazy(() => import('@/views/Test'))
 
 const ROUTES = {
   About: {
@@ -22,10 +21,6 @@ const ROUTES = {
   Contact: {
     element: Contact,
     path: '/contact'
-  },
-  Test: {
-    element: Test,
-    path: '/test'
   }
 }
 
