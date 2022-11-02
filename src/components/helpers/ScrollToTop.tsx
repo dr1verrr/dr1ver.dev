2c0b2f782cb68bd5a0fb01464e71c6a27256104a
dr1verrr <darfor2107@gmail.com>
2022-11-03 01:55:43 +0300

    fix(ScrollToTop): change scrollable ref

--- src/components/helpers/ScrollToTop.tsx
+++ src/components/helpers/ScrollToTop.tsx
@@ -5,14 +5,14 @@ import { useLayoutContext } from '../wrappers/Layout'
 
 function ScrollToTop() {
   const location = useLocation()
-  const { Layout } = useLayoutContext()
+  const { LayoutInner } = useLayoutContext()
   const mounted = useRef(false)
 
   useEffect(() => {
     if (!mounted.current) {
       mounted.current = true
     } else {
-      Layout.ref.current?.scrollTo(0, 0)
+      LayoutInner.ref.current?.scrollTo(0, 0)
     }
   }, [location])
 
